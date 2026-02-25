cbe2742151ce1f691630031395a97f127623f99e
Baileden <baileden@students.zhaw.ch>
2026-02-25 10:16:38 +0100

    Erste Schritte

--- views/home.py
+++ views/home.py
@@ -5,11 +5,11 @@ st.title("Meine erste Streamlit App")
 # !! WICHTIG: Eure Emails müssen in der App erscheinen!!
 
 """
-Diese App wurde von folgenden Personen entwickelt:
-- Anna Müller (anmu@zhaw.ch)
-- Peter Meier (peme@zhaw.ch)
+Diese App wurde von folgender Person entwickelt:
+- Dennis Bailer (baileden@students.zhaw.ch)
+
 
 Diese App ist das leere Gerüst für die App-Entwicklung im Modul Informatik 2 (BMLD/ZHAW)
 
-Autor: Samuel Wehrli (wehs@zhaw.ch)
+Autor: Dennis Bailer (baileden@students.zhaw.ch).
 """
