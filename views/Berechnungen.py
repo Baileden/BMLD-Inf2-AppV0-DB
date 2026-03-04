46e268f6f278c0f71542842a1403f60223836439
Baileden <baileden@students.zhaw.ch>
2026-03-04 10:19:21 +0100

    ...

--- views/Berechnungen.py
+++ views/Berechnungen.py
@@ -12,3 +12,26 @@ with st.form("Addieren"):
     submitted = st.form_submit_button("Berechnen")  
     if submitted:
         st.write("Ergebnis:", add(a, b))
+
+with st.form("Subtrahieren"):
+    st.write("Gib hier deine Zahlen die du subtrahieren willst ein:")
+    a = st.number_input("Nummer 1", key="sub_a")
+    b = st.number_input("Nummer 2", key="sub_b")
+    submitted = st.form_submit_button("Berechnen", key="sub_submit")  
+    if submitted:
+        st.write("Ergebnis:", subtract(a, b))
+
+with st.form("Quadrieren"):
+    st.write("Gib hier deine Zahl die du quadrieren willst ein:")
+    a = st.number_input("Nummer", key="square_a")
+    submitted = st.form_submit_button("Berechnen", key="square_submit")  
+    if submitted:
+        st.write("Ergebnis:", square(a))
+
+with st.form("Wurzel"):
+    st.write("Gib hier deine Zahl und die Wurzel welche daraus gezogen werden soll, ein:")
+    a = st.number_input("Nummer", key="root_a")
+    b = st.number_input("Wurzelgrad", key="root_b")
+    submitted = st.form_submit_button("Berechnen", key="root_submit")  
+    if submitted:
+        st.write("Ergebnis:", root(a, b))
